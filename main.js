68703b00af1e1f2a927499c52e74b42d972d3554
Mistrand <mistrandelisee@gmail.com>
2025-01-21 13:16:22 +0100

    ....

--- main.js
+++ main.js
@@ -106,11 +106,12 @@ function getMax( current , max ) {
     }
   }
 // readTextFile(url);
-// const url = 'https://docs.google.com/document/d/e/2PACX-1vRMx5YQlZNa3ra8dYYxmv-QIQ3YJe8tbI3kqcuC7lQiZm-CSEznKfN_HYNSpoXcZIV3Y_O3YoUB1ecq/pub'
 
 
 (async () => {
   const url = 'https://docs.google.com/document/d/e/2PACX-1vQGUck9HIFCyezsrBSnmENk5ieJuYwpt7YHYEzeNJkIb9OSDdx-ov2nRNReKQyey-cwJOoEKUhLmN9z/pub'
+  // const url = 'https://docs.google.com/document/d/e/2PACX-1vRMx5YQlZNa3ra8dYYxmv-QIQ3YJe8tbI3kqcuC7lQiZm-CSEznKfN_HYNSpoXcZIV3Y_O3YoUB1ecq/pub'
+  
   const {
     matrix,
     output,
